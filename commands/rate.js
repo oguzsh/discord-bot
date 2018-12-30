8fb0c30bfa8d1c43b971fe1d8f1c46774610b72a
Oğuzhan İNCE <oguzhan824@gmail.com>
2018-12-30 14:41:40 +0300

    add poll command"

--- commands/rate.js
+++ commands/rate.js
@@ -4,11 +4,24 @@ exports.run = (client, message, args) => {
     let mesaj = args.slice(0).join(' ');
     if (mesaj.length < 1) return message.reply('Neyi oylayacaksın onu da yaz :/');
     message.delete();
+
     const embed = new Discord.RichEmbed()
+    .setColor("#ffffff")
         .setAuthor('Oylama Başladı')
+        .setTitle(`Oylama tarafından ${message.author.username} başlatıldı`)
         .setColor(3447003)
-        .setDescription(`${mesaj} \n\n\ Evet İçin: :thumbsup: Hayır İçin: :thumbsdown: `)
-    return message.channel.sendEmbed(embed);
+        .setDescription(`${mesaj} \n\n\ Evet İçin: :thumbsup: Hayır İçin: :thumbsdown: `);
+
+        let msg = await message.channel.send(embed)
+            .then(function (msg) {
+                msg.react("❎");
+                msg.react("✅");
+                message.delete({
+                    timeout: 1000
+                });
+            }).catch(function (error) {
+                console.log(error);
+            });
 };
 
 exports.conf = {
