53d73b52f2c435d992772353e39c03e7a9848ad5
Oğuzhan İNCE <oguzhan824@gmail.com>
2018-12-30 15:29:02 +0300

    rate command

--- commands/temizle.js
+++ commands/temizle.js
@@ -3,7 +3,7 @@ exports.run = function (client, message, args) {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
     if (!args[0]) return message.channel.send(" **Lütfen Silinicek Mesaj Miktarını Yazın.!** ");
     message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(` ${args[0]} Adet Mesajı Sildim. ✅`).then(msg => msg.delete(5000));
+        message.channel.send(` ${args[0]} Adet Mesajı Sildim. ✅`).then(msg => msg.delete(2000));
     })
 }
 
