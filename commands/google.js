ec62d34c4a9ec3e0a469f2216c466e52bb9a9075
Oğuzhan İNCE <oguzhan824@gmail.com>
2018-12-31 17:38:13 +0300

    added google func

--- commands/google.js
+++ commands/google.js
@@ -1,53 +1,60 @@
-const Discord = require("discord.js");
-const google = require("google");
+const google = require('google');
+const Discord = require(`discord.js`);
 
-exports.run = async (client, message, args) => {
-
-  google.resultsPerPage = 1
-  google.protocol = 'https'
-  var nextCounter = 0
-
-  google(args, function (err, res) {
-    if (err) console.log(err);
-
-    for (var i = 0; i < res.links.length; ++i) {
-      var link = res.links[i];
-      if (link.title == null) {
-      return void(0)
+exports.run = (client, message) => {
+    let args = message.content.split(/[ ]+/);
+    let suffix = args.slice(1).join(' ');
+    if (!suffix) {
+        message.channel.send({
+            embed: {
+                color: 0x36393F,
+                description: `:warning: **${message.author.username}**, Aramam için birşeyler yazmalısın. \n!!google \`'aramak istediğiniz'\``,
+                footer: {
+                    text: 'Google Arama',
+                }
+            }
+        });
     }
-      if (link.href == null)    {
-      return void(0)
-    }
-      const gEmbed = new Discord.RichEmbed()
-        .setAuthor(`Google search result for ${args}`.split(',').join(' '))
-        .setColor('#ffc1cc')
-        .setThumbnail('http://www.stickpng.com/assets/images/5847f9cbcef1014c0b5e48c8.png')
-        .addField('Website', link.title)
-        .addField('Description', link.description)
-        .addField('URL', link.href);
-
-        message.channel.send(gEmbed);
-    }
-
-    if (nextCounter < 1) {
-      nextCounter += 1
-      if (res.next) res.next()
-    }
-
-  });
-
-}
-
+    google.resultsPerPage = 25;
+    google(suffix, function (err, res) {
+        if (err) message.channel.send({
+            embed: {
+                color: 0xff2727,
+                description: `\n:warning: **${message.author.username}**, lütfen googlede aramak istediğini yaz!`,
+                footer: {
+                    text: 'Google Arama',
+                }
+            }
+        });
+        for (var i = 0; i < res.links.length; ++i) {
+            var link = res.links[i];
+            if (!link.href) {
+                res.next;
+            } else {
+                let embed = new Discord.RichEmbed()
+                    .setColor(`#ffffff`)
+                    .setAuthor(`Sonuç "${suffix}"`, `https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png`)
+                    .setDescription(`\n**Link**: [${link.title}](${link.href})\n**Hakkında Bilgi**:\n${link.description}`)
+                    .setTimestamp()
+                    .setFooter('Google Arama', message.author.displayAvatarURL);
+                return message.channel.send({
+                    embed: embed
+                });
+            }
+            return message.react("✅");
+        }
+    });
+};
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['ara'],
+    aliases: [],
     permLevel: 0
 };
 
 exports.help = {
-  name: "google",
-  description: 'Google da arama yapar',
-  usage: 'google'
+        name: 'google',
+        description: 'Yazdığınız şeyin google en iyi sonucunun linki verir.(yani google de arama yapar)',
+        usage: 'google "aranacak şey"'
 }
